f33e9d19456c7bcb3e2e73d0a2b5d3b7d270981c
Antonio Dias <antoniovictor200@gmail.com>
2022-08-24 15:43:40 -0300

    Inicio do projeto utilizando duas APIs com o mongo

---
+++ Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebApiMongodb.Models
+{
+    public class Produto
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Nome")] //====> Nome da coluna que estará no banco
+        public string Nome { get; set; } = null; 
+    }
+}
